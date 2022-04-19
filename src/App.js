b6c282f6241ef66cdbfbb08645030bde4c2bc6fa
rokamba <rokamba92@gmail.com>
2022-04-18 22:16:49 -0500

    Built the porfolio

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Home from './Home/Home';
+import About from './About/About';
+import Project from './Project/Project';
+import Contact from './Contact/Contact';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <About/>
+      <Project/>
+      <Contact/>
+    
+    </div>
+  );
+}
+
+export default App;
